998e802a7cb517c3aefc2c40051984c3bdca02e0
behradpeikani <behradpeikani@gmail.com>
2023-04-16 11:47:05 +0430

    about us page with comments have created but not comptlely

--- core_app/models.py
+++ core_app/models.py
@@ -6,6 +6,10 @@ class AboutUs(models.Model):
 	title = models.CharField(max_length=155)
 	content = RichTextUploadingField()
 
+	class Meta:
+		verbose_name = 'about us'
+		verbose_name_plural = 'about us'
+
 	def __str__(self):
 		return self.title
 
--- core_app/views.py
+++ core_app/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from .models import AboutUs
+from .models import AboutUs, Comment
+from .forms import CommentForm
 
 
 class AboutUsPage(TemplateView):
 	template_name = 'core_app/about_us_page.html'
+	form_class = CommentForm
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		context['obj'] = AboutUs.objects.all()
+		context['obj'] = AboutUs.objects.first()
+		# context['comments'] = Comment.objects.filter(parent_isnull=True)
+		context['comments'] = Comment.objects.all()
+		context['form'] = self.form_class
+		return context
 
-		return context 
+	def post(self, request, *args, **kwargs):
+		form = self.form_class
+		if form.is_valid():
+			comment = form.save(commit=False)
+			if request.POST.get('reply_to'):
+				comment.parent_id = request.POST.get('reply_to')
+			comment.save()
+			return redirect('about-us/')
+		else:
+			return self.form_invalid(form)
