f7324bd24a96c45064300e994fc062e07d6abc4e
behradpeikani <behradpeikani@gmail.com>
2023-05-14 15:09:06 +0430

    i created tag, category, comment and article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
+import uuid
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=100, blank=True)
+    content = models.RichTextUploadingField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    comment = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
