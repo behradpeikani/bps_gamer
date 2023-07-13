c7ce31d2a7e97eac42dd36fcfa2636299883591a
behradpeikani <behradpeikani@gmail.com>
2023-07-13 19:51:01 +0430

    add custom 404 page

--- core/settings.py
+++ core/settings.py
@@ -3,10 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-aiu-eli54^r2_(xur0t$82&iwqm%1qkkc820yff^qg!99aw9-u'
 
